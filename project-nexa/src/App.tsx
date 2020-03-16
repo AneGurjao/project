e7cbb1a5bdaf0c87e233af6c9fcebc936192aed0
Rosiane-Gurjao <anegurjao@gmail.com>
2020-03-16 01:04:00 -0300

    first commit

---
+++ project-nexa/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+function App() {
+  return (
+    <div className="App">
+        <Typography>
+          Hello word
+        </Typography>
+    </div>
+  );
+}
+
+export default App;
